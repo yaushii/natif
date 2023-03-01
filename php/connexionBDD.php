f250f582034aed5cd02c2ae188b891de9bb62547
Justine <justine.telmann@gmail.com>
2023-03-01 10:36:42 +0100

    modification formulaire

--- php/connexionBDD.php
+++ php/connexionBDD.php
@@ -10,10 +10,9 @@ $dataase="test";
 try {
  
     $pdo = new PDO('mysql:host=localhost;dbname=test', $user , $pass);
-    
+    echo "connexion réussi";
     }
     catch (PDOException $exception) {
-     
      
      echo 'Erreur :'. $exception ->getMessage()."<br/>";
      exit;
--- php/seance.php
+++ php/seance.php
@@ -1,6 +1,4 @@
-<?php
-/*include 'connexionBDD.php'*/
-?>
+
 
 <!DOCTYPE html>
 
@@ -25,60 +23,100 @@
 
     
 
-    <form action="session.php" method="post">
+    <form action="" method="post">
+
     <div class="session-container">
     <p class="tir">Distance de tir (M) :</p>
         <div class="distance">
-        <div class="M"><input class="choix" type="button" value="10"></div>
-        <div class="M"><input class="choix" type="button" value="25"></div>
-        <div class="M"><input class="choix" type="button" value="50"></div>
-        
+
+        <input type="radio" name="dist" id="tir" value="10">
+            <label for="tir" ><img class="dis" src="../images/numero-10.png" alt=""></label>
+
+       <input type="radio" name="dist" id="tir1" value="25">
+            <label for="tir1" ><img class="dis" src="../images/numero-25.png" alt=""></label>
+
+            <input type="radio" name="dist" id="tir2" value="50">
+            <label for="tir2" ><img class="dis" src="../images/50.png" alt=""></label>
 
         </div>
 
+
         <p class="tir">Type de cible (14x14) :</p>
-        <div class="type">
-        
-        <input type="radio" name="sports" id="cible1" Checked>
+
+        <div class="type"> 
+        <input type="radio" name="type" id="cible1" value="../images/cible_14x14_6_small Rouge.png" Checked>
             <label for="cible1" ><img src="../images/cible_14x14_6_small Rouge.png" alt=""></label>
 
-            <input type="radio" name="sports" id="cible2"required>
+            <input type="radio" name="type" id="cible2" value="../images/cible_14x14_10c_small.png">
             <label for="cible2" ><img src="../images/cible_14x14_10c_small.png" alt=""></label>
 
-            <input type="radio" name="sports" id="cible3"required>
+            <input type="radio" name="type" id="cible3" value="../images/cible_14x14_10b_small.png">
             <label for="cible3" ><img src="../images/cible_14x14_10b_small.png" alt=""></label>
 
-            <input type="radio" name="sports" id="cible4"required>
+            <input type="radio" name="type" id="cible4" value="../images/cible_14x14_10a_small.png">
             <label for="cible4" ><img src="../images/cible_14x14_10a_small.png" alt=""></label>
         
-
         </div>
 
         <div class="nbr">
 
         <p class="tir">Nombre de tirs :</p>
 
-
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
-        <input class="plomb" type="image" src="../images/plomb.png" alt="logo!">
+        <input type="checkbox" name="tir[]" id="plomb" >
+            <label for="plomb" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb1" >
+            <label for="plomb1" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb2" >
+            <label for="plomb2" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb3" >
+            <label for="plomb3" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb4" >
+            <label for="plomb4" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb5" >
+            <label for="plomb5" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb6" >
+            <label for="plomb6" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb7" >
+            <label for="plomb7" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb8" >
+            <label for="plomb8" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
+        <input type="checkbox" name="tir[]" id="plomb9" >
+            <label for="plomb9" ><img class="plomb" src="../images/plomb.PNG" alt=""></label>
 
         </div>
 
         <div class="btn">
-        <input class ="btn-btn" type="button" value="envoie">
+        <input class ="btn-btn" name ="valider" type="submit" value="envoie">
         </div>
 
 
 </form>
+<?php
+include 'connexionBDD.php';
+@$dist = $_POST["dist"];
+@$type = $_POST["type"];
+@$tir = $_POST["tir"];
+@$valider=$_POST["valider"];
 
+if(isset($valider)){
+    echo "<br>distance choisie ";
+    echo $dist;
+    echo "<br>type de cible:  ";
+    echo $type;
+    echo "<br> vous avez ";
+    echo sizeof($tir);
+    echo " tir(s) de selectionne";
+
+}
+
+
+
+    if(isset($_POST['distance']) AND isset($_POST['cible']) AND isset($_POST['nbrTr']) )
+    {
+        $requete = $pdo->prepare("INSERT INTO idsession(distance, cible, nbrTr) VALUES(?, ?, ?)");
+        $requete->execute(array(implode ($_POST['distance'],$_POST['cible'],$_POST['tir'])));
+    }
+    ?>
 
 </body>
 
